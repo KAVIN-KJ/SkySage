4723edf26c165849e4ec63be295b323659b7e49c
Madhans57 <madhansundhar57@gmail.com>
2024-08-01 16:33:18 +0530

    maddy

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import TopBar from './TopBar'
 import './App.css'
 
 import WeatherOverview from './WeatherOverview'
+import Chart from './Chart'
+import News from './News'
 const App = () => {
   
   return (
     <div>
       <WeatherOverview/>
-  </div>
+      <News/>
+    </div>
 
   )
 }
+export default App
